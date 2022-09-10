e22a767e3c191cfba4530117cf3c35905908f658
brynnchernosky <56202540+brynnchernosky@users.noreply.github.com>
2022-09-10 17:38:33 -0400

    js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const apiRequest = async () => {
    */
 
   // TODO fill in your own port number 
-  const PORT_NUMBER = "";
+  const PORT_NUMBER = "8010";
 
   const baseUrl = `http://localhost:${PORT_NUMBER}/proxy/api/`
 
@@ -29,8 +29,6 @@ const apiRequest = async () => {
     }
   });
 
-  // console.log(response);
-
   // Return the response in JSON format
   return response.json();
 }
@@ -40,15 +38,23 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15", 
+  fruitList = fruitsArray.map((fruit) => {
+    if (fruit.nutritions.sugar > 5) {
+      return fruit.name
+    }
+  })
 
   // TODO: Create a new HTML element to display your data 
+  const newElement = document.createElement('fruit');
+  newElement.innerHTML = fruitList
 
   // TODO: Append your new element to the page
-
+  const existingElement = document.getElementById('cs1300-gallery');
+  existingElement.append(newElement);
 }
 
 // SAMPLE CODE of how to create and append a new HTML element to the page
@@ -60,4 +66,9 @@ const exampleAddElement = () => {
   // Append the new element to an existing part of the webpage
   const existingElement = document.getElementById('example-id');
   existingElement.append(newElement);
+}
+
+const changeBackgroundColor = () => {
+  const background = document.getElementById('cs1300-gallery');
+  background.style.backgroundColor = 'blue';
 }
